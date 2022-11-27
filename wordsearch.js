a197dfef77da3fe37f996e8f8a29c443e556b8ef
Karen Mui <karen_mui@hotmail.com>
2022-11-27 09:35:07 +0000

    function that searches a matrix vertically and diagonally for a word

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;
     }
-}
+    
+    const newArray = [];
+    for (let i = 0; i < letters.length; i++) {
+        for (let x = 0; x < letters[i].length; x++) {
+            if (i === 0) {
+                newArray.push([letters[0][x]]);
+            } else {
+                newArray[x].push(letters[i][x]);
+            }
+        }
+    }
 
-module.exports = wordSearch
+    const verticalJoin = newArray.map(vs => vs.join(''));
+    for (v of verticalJoin) {
+        if (v.includes(word)) return true;
+    }
+
+    return false;
+};
+
+module.exports = wordSearch;
